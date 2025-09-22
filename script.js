9cc5de830b073fd8b4b271613b2f3a181497e5c8
mourvan <mourvanhmaes@gmail.com>
2025-09-21 21:45:39 -0300

    dados projeto json

---
+++ script.js
@@ -0,0 +1,20 @@
+const pokemons = [
+    {
+        nome: "Pikachu",
+        id: 25,
+        tipo: "eletrico",
+        ht: 0.4,
+        wh: 6,
+        habilidades: ["static", "lightning-rod"],
+        img: "pikachu.png"
+    },
+    {
+    nome: "Charizard",
+    id: 6,
+    tipo: ["fogo", "voador"],
+    ht: 1.7,
+    wh: 90.5,    
+    habilidades: ["blaze", "solar-power"],
+    img: "charizard.png"
+}
+]
